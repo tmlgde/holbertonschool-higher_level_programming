fd5aa53326055914496df08dc3100b02773d689c
tmlgde <tom.lagarde33@outlook.fr>
2025-05-05 08:55:34 +0000

    using print

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""2-print.py - Prints a predefined sentence with proper formatting."""
+
+# This script prints a string with double quotes included.
+# It demonstrates the use of escape characters with print().
+
 print("\"Programming is like building a multilingual puzzle")
 
