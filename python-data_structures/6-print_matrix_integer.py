20af6edce6563b3b2eea8c0ba23055143bcedf28
tmlgde <tom.lagarde33@outlook.fr>
2025-05-12 13:25:48 +0000

    integer matrice

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for ligne in matrix:
         for i, nombre in enumerate(ligne):
-            if i == len(ligne) -1:
+            if i == len(ligne) - 1:
                 print("{:d}".format(nombre), end="")
             else:
                 print("{:d}".format(nombre), end=" ")
