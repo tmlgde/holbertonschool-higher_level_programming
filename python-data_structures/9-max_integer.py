9b510c05b26baf57495686b9cbcdd76b86367bd9
tmlgde <tom.lagarde33@outlook.fr>
2025-05-13 10:49:49 +0000

    bigger number on a tuple

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    bigger = my_list[0]
+
+    for i in  my_list:
+        if i > bigger:
+            bigger = i
+    return bigger
+
