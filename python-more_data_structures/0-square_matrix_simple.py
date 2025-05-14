d8f5dc07b316c8b7c56c1855999fa9c3a601f201
tmlgde <tom.lagarde33@outlook.fr>
2025-05-14 11:58:56 +0000

    square

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
         new_row = []
-        for number in row :
+        for number in row:
             square = number ** 2
             new_row.append(square)
         new_matrix.append(new_row)
