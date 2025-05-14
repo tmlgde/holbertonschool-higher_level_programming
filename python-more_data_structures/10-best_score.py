7d6a5fe766699e29d9e2f311728fd2bd369185c2
tmlgde <tom.lagarde33@outlook.fr>
2025-05-14 13:55:23 +0000

    return the best value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    best_value = max(a_dictionary, key= a_dictionary.get)
+    best_value = max(a_dictionary, key=a_dictionary.get)
     return best_value
