734641f9f88389eec16ee14aada4613e9e4c39cd
tmlgde <tom.lagarde33@outlook.fr>
2025-05-15 14:56:31 +0000

    print only integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,8 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
     print("")
     return count
 
