76e4707da5a0d94018ee5b89a2f5214e26bd5ac1
tmlgde <tom.lagarde33@outlook.fr>
2025-05-15 12:29:24 +0000

    value integer roman

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 def roman_to_int(roman_string):
     my_dict = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
-    if not isinstance (roman_string, str) or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     total = 0
     prev = 0
