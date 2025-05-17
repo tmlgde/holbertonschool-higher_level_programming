ef7850045a4e6232d09a22b82ff6b1e559222cc2
tmlgde <tom.lagarde33@outlook.fr>
2025-05-17 10:53:53 +0200

    division of a list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+
     new_list = []
+
     for i in range(list_length):
         try:
             new_list.append(my_list_1[i] / my_list_2[i])
@@ -14,5 +16,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             new_list.append(0)
         finally:
-                pass
-        return new_list
+            pass
+
+    return new_list
