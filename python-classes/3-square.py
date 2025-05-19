5eae8e2f228df1cc6afa4c23f9127715e5cd08e7
tmlgde <tomlagarde33@outlook.fr>
2025-05-19 12:19:24 +0200

    doc of the area instance

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -31,4 +31,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
