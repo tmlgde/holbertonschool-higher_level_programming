ed2820cd7303f437132232eee1f9ffeaffed4b7b
tmlgde <tomlagarde33@outlook.fr>
2025-05-19 10:47:59 +0200

    create a empty class square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,7 +2,7 @@
 """
 This module defines an empty Square class.
 
-The Square class is a basic geometric shape that currently has no attributes 
+The Square class is a basic geometric shape that currently has no attributes
 or methods. It serves as a simple representation of a square.
 """
 
@@ -10,7 +10,8 @@ class Square:
     """
     An empty class that defines a square.
 
-    This class currently does not have any properties, methods, or data. 
+    This class currently does not have any properties, methods, or data.
     It is used as a foundation for further development.
     """
     pass
+
