2462260d9186daed1ac105837cca55f0dfe8b857
tmlgde <tomlagarde33@outlook.fr>
2025-05-21 11:51:49 +0200

    Getter / setter with property

---
+++ python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines a Square with property and area."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
