eb7e97818b94a46bc137cec201fc252dad2a98ff
tmlgde <tom.lagarde33@outlook.fr>
2025-05-21 14:47:33 +0000

    area self

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -30,6 +30,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """
         Calculates the area of the square.
