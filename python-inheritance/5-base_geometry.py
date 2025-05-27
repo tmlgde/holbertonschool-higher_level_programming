9c7605b2d877d9cce5e944b1b36002c367b319a7
tmlgde <tom.lagarde33@outlook.fr>
2025-05-27 11:34:36 +0000

    create an empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class representing geometry base structure.
+    Intended to be extended in future exercises.
+    """
+    pass
+
