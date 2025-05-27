e5408c93358a590e1a7ac66900b6325551691931
tmlgde <tom.lagarde33@outlook.fr>
2025-05-27 11:42:38 +0000

    raise an exception

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""
+This module defines the BaseGeometry class with an unimplemented area method.
+"""
+
 
 class BaseGeometry:
-    pass
+    """
+    BaseGeometry class intended to be extended by other geometry classes.
+
+    Methods:
+        area(self): Raises an Exception indicating it is not implemented.
+    """
+
     def area(self):
+        """
+        Raises an exception to indicate that the method is not yet implemented.
+
+        This method should be overridden by subclasses.
+        """
         raise Exception("area() is not implemented")
