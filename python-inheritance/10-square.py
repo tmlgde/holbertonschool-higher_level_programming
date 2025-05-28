10d847e618fd869e22401a0a8bb7596d55747d17
tmlgde <tom.lagarde33@outlook.fr>
2025-05-28 09:20:39 +0000

    using super for take methods from rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
 """
-This module defines a Square class that inherits from Rectangle.
+Définition de la classe Square, héritée de Rectangle.
 """
 
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """
-    Square class that inherits from Rectangle.
-
-    Instantiated with a single size value, validated as a positive integer.
-    """
+    """Classe représentant un carré, héritée de Rectangle."""
 
     def __init__(self, size):
-        """
-        Initialize a Square with size, validated as a positive integer.
-
-        Args:
-            size (int): The size of the square (must be > 0).
-        """
+        """Initialise un carré avec une taille unique."""
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
-
+        self.__size = size
