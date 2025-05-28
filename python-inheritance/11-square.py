6f92515bdfb791c70c0f20b4eb606c5d002563ca
tmlgde <tom.lagarde33@outlook.fr>
2025-05-28 09:24:40 +0000

    using __str__

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Définition de la classe Square, héritée de Rectangle.
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Classe représentant un carré, héritée de Rectangle."""
+
+    def __init__(self, size):
+        """Initialise un carré avec une taille unique."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Retourne une représentation du carré."""
+        return f"[Square] {self.__size}/{self.__size}"
