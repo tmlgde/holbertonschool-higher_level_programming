87ed76086f8884ee5b49fbb464cbbca12b10759e
tmlgde <tom.lagarde33@outlook.fr>
2025-05-29 10:47:50 +0000

    iter, for iterable

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-"""Module defining CountedIterator, an iterator that counts how many
-items have been returned during iteration.
+"""Module defining CountedIterator:
+an iterator that counts how many items have been returned.
 """
 
 
 class CountedIterator:
-    """Custom iterator that counts how many items have been iterated."""
+    """Custom iterator that wraps an iterable and counts iterations."""
 
     def __init__(self, iterable):
-        """Initialize with an iterable and set the count to zero."""
+        """Initialize the iterator and the counter."""
         self.iterator = iter(iterable)
         self.count = 0
 
     def __next__(self):
-        """Return the next item and increment the count."""
+        """Return the next item and increment the counter."""
         self.count += 1
         item = next(self.iterator)
         return item
