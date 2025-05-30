e4dec0700d0f4b504d82bbd48b435869145fafcd
tmlgde <tom.lagarde33@outlook.fr>
2025-05-30 08:22:53 +0000

    new file for exo 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 """
-This module provides a class BaseGeometry
+Définition d'une classe de base pour la géométrie.
+
+Contient :
+- une méthode area non implémentée,
+- une méthode integer_validator pour valider les entiers strictement positifs.
 """
 
 
 class BaseGeometry:
-    """
-    This class is about geometry
-    """
+    """Classe de base pour la géométrie."""
 
     def area(self):
-        """
-        Raise an exception as area is not implemented
-        """
-
+        """Lève une exception car la méthode n'est pas implémentée."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validates value
-        """
+        """Valide que value est un entier strictement positif.
 
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+        Args:
+            name (str): Le nom de la valeur.
+            value (int): La valeur à valider.
+
+        Raises:
+            TypeError: si value n'est pas un entier.
+            ValueError: si value est <= 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
