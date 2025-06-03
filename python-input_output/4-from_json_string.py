a85b75fd2918834dd261f5627e34b36c794824d7
tmlgde <tom.lagarde33@outlook.fr>
2025-06-03 09:38:21 +0000

    return on python

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-"""4. From JSON string to Object"""
+"""4. Read file"""
 
 
 def from_json_string(my_str):
-    """
-    Converts a JSON string to a Python object.
-
-    Args:
-        my_str (str): The JSON string to convert.
-
-    Returns:
-        object: The corresponding Python data structure.
-    """
+    """Convert a JSON string to a Python object."""
     import json
-    return json.loads(my_str)
 
+    return json.loads(my_str)
