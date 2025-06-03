8bafc8a411c4b212c0f66342fc3d9f25daa6eb0d
tmlgde <tom.lagarde33@outlook.fr>
2025-06-03 15:41:49 +0000

    add new file exo 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""7. Load, add, save"""
+"""Script qui ajoute tous les arguments passés à la ligne de commande
+à une liste JSON sauvegardée dans le fichier add_item.json.
+Utilise les fonctions save_to_json_file et load_from_json_file.
+"""
 
-import sys        # Pour récupérer les arguments passés en ligne de commande
-import os         # Pour vérifier si le fichier existe
-from save_to_json_file import save_to_json_file   # Fonction pour sauvegarder une liste en JSON
-from load_from_json_file import load_from_json_file  # Fonction pour charger une liste depuis un fichier JSON
+from sys import argv
 
-filename = "add_item.json"  # Nom du fichier où la liste est stockée
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-# Vérifie si le fichier existe pour le charger, sinon initialise une liste vide
-if os.path.exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
+conteneur = argv[1:]
 
-# Ajoute les arguments passés en ligne de commande à la liste
-my_list.extend(sys.argv[1:])
+try:
+    pelo = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    pelo = []
 
-# Sauvegarde la liste mise à jour dans le fichier JSON
-save_to_json_file(my_list, filename)
+pelo.extend(conteneur)
+save_to_json_file(pelo, "add_item.json")
