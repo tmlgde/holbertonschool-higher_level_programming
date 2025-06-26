5c74f908a9bf1221c4d39723f370a1ead9a2cb4b
tmlgde <tom.lagarde33@outlook.fr>
2025-06-26 12:42:50 +0000

    list all show about comedy

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,8 @@
--- list all shows on comedy
+-- 15 only comedy
+-- 15 Comedy only
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_show.title ASC;
+ORDER BY tv_shows.title ASC;
