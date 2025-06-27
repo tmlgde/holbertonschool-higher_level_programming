7c1ec001d48b4c97a6d31169753ca7e60cc781b4
tmlgde <tom.lagarde33@outlook.fr>
2025-06-27 11:34:29 +0000

    add class and model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,10 +1,18 @@
+#!/usr/bin/python3
+"""
+Import classes that inherit from Base
+"""
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class State(Base):
-    __tablename__: "states"
 
+class State(Base):
+    """
+    Class for State
+    """
+
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
