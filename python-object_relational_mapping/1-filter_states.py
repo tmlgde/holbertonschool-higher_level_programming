fd8b6989222a675c41cc4e7ab56ecd331904ea75
tmlgde <tom.lagarde33@outlook.fr>
2025-06-27 09:18:25 +0000

    list all with a word start by N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 """
 Lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa.
+from the database hbtn_0e_0_usa
 """
-
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Connect to database
+    """
+    Prints all states starting with N and sorted by id
+    """
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states "
+        "WHERE BINARY name LIKE 'N%' "
+        "ORDER BY id ASC"
+    )
 
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
