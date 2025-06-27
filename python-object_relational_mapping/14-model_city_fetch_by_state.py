6222718dd6bdc2a6484bf11c7447cd5e2c0093e7
tmlgde <tom.lagarde33@outlook.fr>
2025-06-27 13:24:43 +0000

    add a new class and print with both classes

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import Base, City
 import sys
+from model_state import Base, State
+from model_city import City
 
 if __name__ == "__main__":
+    """
+    Connects to the database and lists all State objects,
+    sorted by id in ascending order.
+    """
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
@@ -17,9 +23,14 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(City, State).join(State).order_by(City.id).all()
+    results = (
+        session.query(State.name, City.id, City.name)
+        .join(City)
+        .order_by(City.id)
+        .all()
+    )
 
-    for city, state in cities:
-        print(f"{state.name}: ({city.id}) {city.name}")
+    for state_name, city_id, city_name in results:
+        print(f"{state_name}: ({city_id}) {city_name}")
 
     session.close()
