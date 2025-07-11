a6e56b5de1282a2182c79931e39ee8ddb8d2781f
tmlgde <tom.lagarde33@outlook.fr>
2025-07-11 11:15:47 +0200

    lenght to length

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const args = process.argv.slice(2)
 
-if (args.lenght === 0) {
+if (args.length === 0) {
     console.log('No argument');
-} else if (args.lenght === 1) {
+} else if (args.length === 1) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
