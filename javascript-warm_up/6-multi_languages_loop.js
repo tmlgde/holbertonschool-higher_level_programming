a0648b27683f5664e588dbf5680727f54949554e
tmlgde <tom.lagarde33@outlook.fr>
2025-07-11 12:28:31 +0200

    using array for 1 console.log

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const tableau = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing',
+];
+for (let i = 0; i < tableau.length; i++) {
+    console.log(tableau[i]);
+}
