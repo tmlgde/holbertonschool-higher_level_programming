217dd49bea386a565ab501adce6b22fbd71fb8c7
tmlgde <tom.lagarde33@outlook.fr>
2025-07-17 12:24:45 +0200

    first step for task 3

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import json
+import csv
+
+app = Flask(__name__)
+
+@app.route('/products')
+def products():
+    source = request.args.get("source")
+    
+    if source=json:
+        with open('products.json', "r") as file:
+            data = json.load(file)
+            return str(data)
+    elif source == "csv":
+        with open('products.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            data = list(reader)
+    else:
+        return "Wrong Source"                
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
